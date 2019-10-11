decd748afebf613e45aafcc7357f20fd0c195878
Kyle Leonard <kdleonard@icloud.com>
2019-10-10 19:23:18 -0500

    splay for buttons on Dashboard.
    Now working on logic

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Dashboard() {
-  const Calls = () => {
-    return ();
-  };
+  const [strikes, fouls, balls, hits] = useState(0);
+  return (
+    <div>
+      <button className="strikes" onClick="">
+        Strike
+      </button>
+      <button className="fouls" onClick="">
+        Foul
+      </button>
+      <button className="balls" onClick="">
+        Ball
+      </button>
+      <button className="hits" onClick="">
+        Hit
+      </button>
+    </div>
+  );
 }
 
 export default Dashboard;
