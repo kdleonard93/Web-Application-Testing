2bcba2b630b3a6ed2f513de97d168c0fb909fc24
Kyle Leonard <kdleonard@icloud.com>
2019-10-10 19:16:14 -0500

    Set up the display and started on Dashboards

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,1 +1,9 @@
 import React from "react";
+
+function Dashboard() {
+  const Calls = () => {
+    return ();
+  };
+}
+
+export default Dashboard;
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,1 +1,18 @@
 import React from "react";
+import { ballCount, strikeCount } from "./Dashboard";
+
+function Display(props) {
+  return (
+    <div>
+      <h1>Player</h1>
+      <h2>Balls</h2>
+      <h4>{ballCount}</h4>
+      <span />
+      <h1>Player</h1>
+      <h2>Strikes</h2>
+      <h4>{strikeCount}</h4>
+    </div>
+  );
+}
+
+export default Display;
