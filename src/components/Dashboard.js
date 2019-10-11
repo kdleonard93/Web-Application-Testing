c23b9882fa0f0e4fed882d196b2b1df60eac813d
Kyle Leonard <kdleonard@icloud.com>
2019-10-10 19:34:28 -0500

    Logic in progress....

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 
+export const ballCount = balls => {
+  if (balls >= 2) {
+  }
+};
+
 function Dashboard() {
   const [strikes, fouls, balls, hits] = useState(0);
   return (
